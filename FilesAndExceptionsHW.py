bd51996915d21e2200f3c773f35c8a363dd9cb2f
A02326366 <A02326366@alabama.edu>
2024-11-06 15:22:13 -0600

    Add files via upload

--- FilesAndExceptionsHW.py
+++ FilesAndExceptionsHW.py
@@ -3,29 +3,53 @@ def main():
     line = ''
     total = 0
     average = 0
+    count = 0
 
-    #open the numbers.txt file to read
-    infile = open('numbers.txt', 'r')
+    try:
+        fileName = input('Enter the name of the file: ')
+        #open the numbers.txt file to read
+        infile = open(fileName, 'r')
 
-    #read the data
-    line = infile.readline()
+        #read the data
+        line = infile.readline()
+        count = 1
 
     
-    while line != '':
-        #strip the line feed
-        line  = line.rstrip('\n')
-        #add up the numbers
-        total = total + int(line)
-        #restart line
-        line = infile.readline()
+        while line != '':
+            try:
+                #strip the line feed
+                line  = line.rstrip('\n')
+                
+                #add up the numbers
+                total = total + int(line)
 
+                #print the line to see the contents
+                print(line)
+                
+                #restart line
+                line = infile.readline()
 
+                #add to counter when line is read
+                count += 1
+
+                #find the average
+                average = int(total / count)
+    
+            except ValueError:
+                print(f"ERROR: Could not convert '{line}' to a number.")
+
+    except IOError:
+        print('ERROR: Could not open or read the file')
+
+    except FileNotFoundError:
+        print('ERROR: The specified file could not be found')
+
+    
     #print total
-    print('The sum of the numbers is ' , total)
+    print('The sum of the numbers is' , total)
+    print('The average of the numbers is' , average)
+    
     #close the file
     infile.close()
-
-    #print contents
-    print(contents)
 
 main()
