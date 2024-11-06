ebf7f6f79fc0ac9d452ac43b56ffc31f0130156d
A02326366 <A02326366@alabama.edu>
2024-11-06 13:53:45 -0600

    Add files via upload

--- FilesAndExceptionsHW.py
+++ FilesAndExceptionsHW.py
@@ -1,13 +1,27 @@
 def main():
-    #define local variable
-    contents = ''
+    #define local variables
+    line = ''
+    total = 0
+    average = 0
 
     #open the numbers.txt file to read
     infile = open('numbers.txt', 'r')
 
     #read the data
-    contents = infile.read()
+    line = infile.readline()
 
+    
+    while line != '':
+        #strip the line feed
+        line  = line.rstrip('\n')
+        #add up the numbers
+        total = total + int(line)
+        #restart line
+        line = infile.readline()
+
+
+    #print total
+    print('The sum of the numbers is ' , total)
     #close the file
     infile.close()
 
